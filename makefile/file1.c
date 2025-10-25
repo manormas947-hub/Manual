bb740f4291c918cfca80178b519e5e0c76a9e90a
112502005 <112502005@iitpkd.smail.ac.in>
2025-10-25 16:23:52 +0530

    added files

---
+++ makefile/file1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int add(int a,int b);
+
+int main(){
+    int x=2,y=0;
+    printf("sum:%d\n",add(x,y));
+    printf("this file depend upon file2\n");
+    return 0;
+}
---
+++ makefile/file3.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+    printf("This is a separate file does not depend on file1 and file 2\n");
+    return 0;
+}
